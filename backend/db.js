b8e8c2cd35e9684658dfb38f2a78389c935b965a
Eloisa Cavalaro <eloisa.cavalaro@gmail.com>
2026-08-29 00:40:01 -0300

    M

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
