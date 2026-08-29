4ce703362cbdc6e927652e36ea663775d8b03d62
Eloisa Cavalaro <eloisa.cavalaro@gmail.com>
2026-08-29 00:17:20 -0300

    MA2

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ const PORT = process.env.PORT || 3000;
 
 
 const corsOptions = {
-    origin: "http://127.0.0.1:5500"
+    origin: "https://martins-atelie-api.onrender.com"
 };
 
 app.use(cors(corsOptions));
