42532ddbf505df07fff7df8cba5cecb6961176df
Eloisa Cavalaro <eloisa.cavalaro@gmail.com>
2026-08-30 00:35:48 -0300

    Atualizações

--- backend/server.js
+++ backend/server.js
@@ -118,7 +118,7 @@ app.use(limiteGeral);
 
 const limiteLogin = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 5,
+    limit: 10,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
